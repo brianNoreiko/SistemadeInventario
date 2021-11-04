f42c4c6f660736c01e8bf6dbdb7dc779d0de1aae
Kessler1459 <kessler1459@gmail.com>
2021-11-04 12:10:26 -0300

    agregar lo del mail aca..

--- SI/src/main/java/com/example/demo/controller/MovementController.java
+++ SI/src/main/java/com/example/demo/controller/MovementController.java
@@ -2,10 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.models.Movement;
 import com.example.demo.models.Product;
+import com.example.demo.models.Stat;
 import com.example.demo.models.dto.MovementDTO;
 import com.example.demo.models.responses.Response;
 import com.example.demo.service.MovementService;
 import com.example.demo.service.ProductService;
+import com.example.demo.service.StatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,31 +15,38 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import utils.EntityResponse;
 import utils.EntityURLBuilder;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200",methods = {RequestMethod.POST,RequestMethod.GET})
+@CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.POST, RequestMethod.GET})
 @RestController
 @RequestMapping("/movements")
 public class MovementController {
     final MovementService movementService;
     final ProductService productService;
+    final StatService statService;
 
     @Autowired
-    public MovementController(MovementService movementService, ProductService productService) {
+    public MovementController(MovementService movementService, ProductService productService, StatService statService) {
         this.movementService = movementService;
         this.productService = productService;
+        this.statService = statService;
     }
 
     @PostMapping
     public ResponseEntity<Response> createMovement(@RequestBody MovementDTO movement) {
         Product pr = productService.getByBarcode(movement.getBarcode());
         Movement newMov = Movement.builder().date(LocalDateTime.now()).product(pr).quantity(movement.getQuantity()).build();
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .contentType(MediaType.APPLICATION_JSON)
-                .location(EntityURLBuilder.buildURL("movements", movementService.create(newMov).getId()))
-                .body(EntityResponse.messageResponse("Movement created successfully"));
+        Stat stats = statService.getStats(pr);
+        if (pr.getUnits() + newMov.getQuantity() <= stats.getReorder()){
+            //TODO SI LA CANTIDAD QUE QUEDA DESPUES DEL RETIRO ES IGUAL O MENOR AL PUNTO DE REORDEN ENVIAR MAIL DE AVISO
+        }
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .location(EntityURLBuilder.buildURL("movements", movementService.create(newMov).getId()))
+                    .body(EntityResponse.messageResponse("Movement created successfully"));
     }
 
     @GetMapping
